2063a80ed0ae480decfd77d44ee1d8dbbeef59cd
Mallikarjuna S J <msj2@users.noreply.github.com>
2018-08-08 15:00:55 +0530

    year_class.java
    Use as many methods in year class,

---
+++ year_class.java
@@ -0,0 +1,22 @@
+/*
+https://docs.oracle.com/javase/8/docs/api/allclasses-noframe.html has all classes in JDK 1.8
+
+https://docs.oracle.com/javase/8/docs/api/java/time/Year.html
+
+ALl methods of Class Year
+
+Ok, fall to feet of tutorialspoint
+tps://www.tutorialspoint.com/javatime/javatime_year.htm
+*/
+
+import java.time.Year;
+
+public class year_1
+{
+  public static void main(String[] args)
+  {
+  Year y = Year.of(2018); // using new ives Errpr
+  System.out.println(y.getValue());
+  // System.out.println(y.getLong(2018));
+  }
+ }
